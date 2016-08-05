a772f3cb90f6028ce0a1774bc7905503a650e428
Konstantin Sukharev <poed@me.com>
2016-08-05 20:13:23 +0300

    Remove weakify/unown overloads

--- Fx/Weakify.swift
+++ Fx/Weakify.swift
@@ -4,33 +4,9 @@
 public func weakify<A: AnyObject, B, C>(x: A, _ f: A -> B -> C) -> B -> C? {
 	return { [weak x] y in x.map{f($0)(y)} }
 }
-/// Curried, ignores B, returns C?
-public func weakify<A: AnyObject, B, C>(x: A, _ f: A -> () -> C) -> B -> C? {
-	return { [weak x] _ in x.map{f($0)()} }
-}
 /// Uncurried, returns C?
 public func weakify<A: AnyObject, B, C>(x: A, _ f: (A, B) -> C) -> B -> C? {
 	return { [weak x] y in x.map{f($0, y)} }
-}
-/// Uncurried, ignores B, returns C?
-public func weakify<A: AnyObject, B, C>(x: A, _ f: A -> C) -> B -> C? {
-	return { [weak x] _ in x.map(f) }
-}
-/// Curried, returns Void
-public func weakify<A: AnyObject, B>(x: A, _ f: A -> B -> ()) -> B -> () {
-	return { [weak x] y in _ = x.map{f($0)(y)} }
-}
-/// Curried, ignores B, returns Void
-public func weakify<A: AnyObject, B>(x: A, _ f: A -> () -> ()) -> B -> () {
-	return { [weak x] _ in _ = x.map{f($0)()} }
-}
-/// Uncurried, returns Void
-public func weakify<A: AnyObject, B>(x: A, _ f: (A, B) -> ()) -> B -> () {
-	return { [weak x] y in _ = x.map{f($0, y)} }
-}
-/// Uncurried, ignores B, returns Void
-public func weakify<A: AnyObject, B>(x: A, _ f: A -> ()) -> B -> () {
-	return { [weak x] _ in _ = x.map(f) }
 }
 
 // MARK: Unown
@@ -38,15 +14,7 @@ public func weakify<A: AnyObject, B>(x: A, _ f: A -> ()) -> B -> () {
 public func unown<A: AnyObject, B, C>(x: A, _ f: A -> B -> C) -> B -> C {
 	return { [unowned x] y in f(x)(y) }
 }
-/// Curried, ignores B
-public func unown<A: AnyObject, B, C>(x: A, _ f: A -> () -> C) -> B -> C {
-	return { [unowned x] _ in f(x)() }
-}
 /// Uncurried
 public func unown<A: AnyObject, B, C>(x: A, _ f: (A, B) -> C) -> B -> C {
 	return { [unowned x] y in f(x, y) }
-}
-/// Uncurried, ignores B
-public func unown<A: AnyObject, B, C>(x: A, _ f: A -> C) -> B -> C {
-	return { [unowned x] _ in f(x) }
 }
