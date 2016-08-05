611536049344c5a46654bdcdfb797c9afd1bd2f7
Konstantin Sukharev <poed@me.com>
2016-08-05 20:12:50 +0300

    Add throttle()

--- Fx/Throttle.swift
+++ Fx/Throttle.swift
@@ -1,23 +1,22 @@
-//import BrightFutures
-//
-//public func throttle<A>(interval: NSTimeInterval, on context: ExecutionContext = Queue.main.context, function f: A -> ()) -> (A -> ()) {
-//	/// FIXME: Replace Shared with Atomic for thread safety
-//	let blockRef = Shared<dispatch_block_t?>(value: nil)
-//
-//	return { x in
-//
-//		let block = dispatch_block_create(DISPATCH_BLOCK_INHERIT_QOS_CLASS) {
-//			context {
-//				blockRef.value = nil
-//				f(x)
-//			}
-//		}
-//
-//		dispatch_block_cancel <^> blockRef.value
-//		blockRef.value = block
-//
-//		let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(interval * Double(NSEC_PER_SEC)))
-//		let globalQueue = dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0)
-//		dispatch_after(delayTime, globalQueue, block)
-//	}
-//}
+
+public func throttle<A>(interval: NSTimeInterval, on queue: dispatch_queue_t = dispatch_get_main_queue(), function f: A -> ()) -> (A -> ()) {
+	/// TODO: Replace Shared with Atomic for thread safety
+	var blockRef = Shared<dispatch_block_t?>(nil)
+
+	return { x in
+
+		let block = dispatch_block_create(DISPATCH_BLOCK_INHERIT_QOS_CLASS) {
+			dispatch_async(queue) {
+				blockRef.value = nil
+				f(x)
+			}
+		}
+
+		dispatch_block_cancel <^> blockRef.value
+		blockRef.value = block
+
+		let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(interval * Double(NSEC_PER_SEC)))
+		let globalQueue = dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0)
+		dispatch_after(delayTime, globalQueue, block)
+	}
+}
