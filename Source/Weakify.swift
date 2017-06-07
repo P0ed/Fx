c84b8c900adc4fdd39e7e0aca36d730f329683ed
Konstantin Sukharev <poed@me.com>
2017-06-07 14:47:00 +0300

    Update weakify, unown functions

--- Source/Weakify.swift
+++ Source/Weakify.swift
@@ -1,20 +1,8 @@
 
-// MARK: Weakify
-/// Curried, returns C?
-public func weakify<A: AnyObject, B, C>(_ x: A, _ f: @escaping (A) -> (B) -> C) -> (B) -> C? {
-	return { [weak x] y in x.map{f($0)(y)} }
-}
-/// Uncurried, returns C?
-public func weakify<A: AnyObject, B, C>(_ x: A, _ f: @escaping (A, B) -> C) -> (B) -> C? {
-	return { [weak x] y in x.map{f($0, y)} }
+public func weakify<A: AnyObject, B, C>(_ x: A, _ f: @escaping (A.Type) -> (A) -> (B) -> C) -> (B) -> C? {
+	return { [weak x] y in x.map{f(A.self)($0)(y)} }
 }
 
-// MARK: Unown
-/// Curried
-public func unown<A: AnyObject, B, C>(_ x: A, _ f: @escaping (A) -> (B) -> C) -> (B) -> C {
-	return { [unowned x] y in f(x)(y) }
-}
-/// Uncurried
-public func unown<A: AnyObject, B, C>(_ x: A, _ f: @escaping (A, B) -> C) -> (B) -> C {
-	return { [unowned x] y in f(x, y) }
+public func unown<A: AnyObject, B, C>(_ x: A, _ f: @escaping (A.Type) -> (A) -> (B) -> C) -> (B) -> C {
+	return { [unowned x] y in f(A.self)(x)(y) }
 }
