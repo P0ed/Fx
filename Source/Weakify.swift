c9676e688c13cc5b3a96026c1def5ef2d5daf7db
Konstantin Sukharev <poed@me.com>
2017-06-23 14:31:11 +0300

    Add weak, unowned functions

--- Source/Weakify.swift
+++ Source/Weakify.swift
@@ -1,6 +1,13 @@
+public func weak<A: AnyObject>(_ x: A) -> () -> A? {
+	return { [weak x] in x }
+}
 
 public func weakify<A: AnyObject, B, C>(_ x: A, _ f: @escaping (A.Type) -> (A) -> (B) -> C) -> (B) -> C? {
 	return { [weak x] y in x.map{f(A.self)($0)(y)} }
+}
+
+public func unowned<A: AnyObject>(_ x: A) -> () -> A {
+	return { [unowned x] in x }
 }
 
 public func unown<A: AnyObject, B, C>(_ x: A, _ f: @escaping (A.Type) -> (A) -> (B) -> C) -> (B) -> C {
