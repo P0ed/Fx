d0de69a8593da16cec1c9dbf4149eabbdbc07271
Konstantin Sukharev <poed@me.com>
2018-05-24 20:19:09 +0300

    Add one more •

--- Source/Application+Composition.swift
+++ Source/Application+Composition.swift
@@ -9,6 +9,13 @@ public func § <A, B> (f: (A) -> B, x: A) -> B {
 public func • <A, B, C> (f: @escaping (B) -> C, g: @escaping (A) -> B) -> (A) -> C {
 	return { x in f(g(x)) }
 }
+
+@_transparent
+public func • <B, C> (f: @escaping (B) -> C, g: @escaping () -> B) -> () -> C {
+	return { f(g()) }
+}
+
+@_transparent
 public func • (f: @escaping VoidFunc, g: @escaping VoidFunc) -> VoidFunc {
 	return { g(); f() }
 }
