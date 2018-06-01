d765e3c50ff895180b2daf7006e4b937cc8e5108
Konstantin Sukharev <poed@me.com>
2018-06-01 13:36:03 +0300

    Rethink weakify

--- Source/Weakify.swift
+++ Source/Weakify.swift
@@ -6,12 +6,12 @@ public func unowned<A: AnyObject>(_ x: A) -> () -> A {
 	return { [unowned x] in x }
 }
 
-public func weakify<A: AnyObject, B, C>(_ x: A, _ f: @escaping (A.Type) -> (A) -> (B) -> C) -> (B) -> C? {
-	return { [weak x] y in x.map{f(A.self)($0)(y)} }
+public func weakify<A: AnyObject, B>(_ x: A, _ f: @escaping (A, B) -> Void) -> (B) -> Void {
+	return { [weak x] y in x.map { x in f(x, y) } }
 }
 
-public func weakify<A: AnyObject, C>(_ x: A, _ f: @escaping (A.Type) -> (A) -> () -> C) -> () -> C? {
-	return { [weak x] in x.map{f(A.self)($0)()} }
+public func weakify<A: AnyObject>(_ x: A, _ f: @escaping (A) -> Void) -> () -> Void {
+	return { [weak x] in x.map { x in f(x) } }
 }
 
 public func weakify<A: AnyObject, B>(_ x: A, _ f: @escaping (A.Type) -> (A) -> (B) -> Void) -> (B) -> Void {
