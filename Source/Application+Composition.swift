94531eb2705dfd62b4327fd79c3fb46c89c20983
Konstantin Sukharev <poed@me.com>
2018-07-05 20:09:08 +0300

    Update fn composition, remove @_transparent attribute

--- Source/Application+Composition.swift
+++ Source/Application+Composition.swift
@@ -1,21 +1,30 @@
 /// Function application
-@_transparent
-public func § <A, B> (f: (A) -> B, x: A) -> B {
-	return f(x)
+public func § <A, B> (f: (A) throws -> B, x: A) rethrows -> B {
+	return try f(x)
 }
 
 /// Function composition
-@_transparent
 public func • <A, B, C> (f: @escaping (B) -> C, g: @escaping (A) -> B) -> (A) -> C {
 	return { x in f(g(x)) }
 }
-
-@_transparent
+/// Function composition
 public func • <B, C> (f: @escaping (B) -> C, g: @escaping () -> B) -> () -> C {
 	return { f(g()) }
 }
-
-@_transparent
-public func • (f: @escaping VoidFunc, g: @escaping VoidFunc) -> VoidFunc {
+/// Function composition
+public func • (f: @escaping () -> Void, g: @escaping () -> Void) -> () -> Void {
 	return { g(); f() }
+}
+
+/// Function composition
+public func • <A, B, C> (f: @escaping (B) throws -> C, g: @escaping (A) throws -> B) -> (A) throws -> C {
+	return { x in try f(g(x)) }
+}
+/// Function composition
+public func • <B, C> (f: @escaping (B) throws -> C, g: @escaping () throws -> B) -> () throws -> C {
+	return { try f(g()) }
+}
+/// Function composition
+public func • (f: @escaping () throws -> Void, g: @escaping () throws -> Void) -> () throws -> Void {
+	return { try g(); try f() }
 }
--- Source/Functions.swift
+++ Source/Functions.swift
@@ -1,16 +1,13 @@
 /// The identity function; returns its argument.
-@_transparent
 public func id<A>(_ x: A) -> A {
 	return x
 }
 
 /// Returns a function which ignores its argument and returns `x` instead
-@_transparent
 public func const<A, B>(_ x: B) -> (A) -> B {
 	return { _ in x }
 }
 /// Returns a function which returns `x`
-@_transparent
 public func const<A>(_ x: A) -> () -> A {
 	return { x }
 }
@@ -42,24 +39,20 @@ public func flip<A, B, C>(_ f: @escaping (A) throws -> (B) throws -> C) -> (B) -
 }
 
 /// Converts () -> B func into A -> B by ignoring input argument
-@_transparent
 public func ignoreInput<A, B>(_ f: @escaping () -> B) -> (A) -> B {
 	return { _ in f() }
 }
 /// Converts A -> B func into A -> () by ignoring result
-@_transparent
 public func ignoreOutput<A, B>(_ f: @escaping (A) -> B) -> (A) -> () {
 	return { x in _ = f(x) }
 }
 
 /// Alias for withExtendedLifetime function
-@_transparent
 public func capture(_ value: Any) {
 	withExtendedLifetime(value, {})
 }
 
 /// Atomically mutates value
-@_transparent
 public func modify<A>(_ value: inout A, f: (inout A) throws -> Void) rethrows {
 	var copy = value
 	try f(&copy)
@@ -67,7 +60,6 @@ public func modify<A>(_ value: inout A, f: (inout A) throws -> Void) rethrows {
 }
 
 /// Returns mutated copy of value
-@_transparent
 public func modify<A>(_ value: A, f: (inout A) throws -> Void) rethrows -> A {
 	var copy = value
 	try f(&copy)
