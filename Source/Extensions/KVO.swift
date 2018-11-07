442fb293c01d86b3a6516d08344513fcccbf461c
Konstantin Sukharev <poed@me.com>
2018-11-07 14:28:15 +0300

    Fix KVO for enums

--- Source/Extensions/KVO.swift
+++ Source/Extensions/KVO.swift
@@ -6,10 +6,8 @@ public extension NSObjectProtocol where Self: NSObject {
 		return Property<Value>(
 			value: self[keyPath: keyPath],
 			signal: Signal<Value> { didChange in
-				let observation = observe(keyPath, options: .new) { _, change in
-					// The guard is because of https://bugs.swift.org/browse/SR-6066
-					guard let newValue = change.newValue else { return }
-					didChange(newValue)
+				let observation = observe(keyPath, options: .new) { object, _ in
+					didChange(object[keyPath: keyPath])
 				}
 				return ActionDisposable(action: { capture(self) } • observation.invalidate)
 			}
