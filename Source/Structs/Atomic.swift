58fdc0e749d321745cc0193d0f95c50d03da6728
Konstantin Sukharev <poed@me.com>
2019-03-26 16:12:41 +0300

    Replace OS_SPINLOCK w/ UnfairLock

--- Source/Structs/Atomic.swift
+++ Source/Structs/Atomic.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// An atomic variable.
 public final class Atomic<Value> {
-	private var spinLock = OS_SPINLOCK_INIT
+	private var lock = UnfairLock()
 	private var _value: Value
 
 	/// Atomically gets or sets the value of the variable.
@@ -16,33 +16,20 @@ public final class Atomic<Value> {
 		_value = value
 	}
 
-	private func lock() {
-		OSSpinLockLock(&spinLock)
-	}
-
-	private func unlock() {
-		OSSpinLockUnlock(&spinLock)
-	}
-
 	/// Atomically replaces the contents of the variable.
 	///
 	/// Returns the old value.
 	public func swap(_ newValue: Value) -> Value {
-		lock()
-		defer { unlock() }
-
-		let oldValue = _value
-		_value = newValue
-
-		return oldValue
+		return lock.with {
+			let oldValue = _value
+			_value = newValue
+			return oldValue
+		}
 	}
 
 	/// Atomically modifies the variable.
 	public func modify(_ f: (inout Value) -> ()) {
-		lock()
-		defer { unlock() }
-
-		Fx.modify(&_value, f)
+		lock.with { Fx.modify(&_value, f) }
 	}
 
 	/// Atomically performs an arbitrary action using the current value of the
@@ -50,9 +37,48 @@ public final class Atomic<Value> {
 	///
 	/// Returns the result of the action.
 	public func withValue<A>(_ f: (Value) -> A) -> A {
+		return lock.with { f(_value) }
+	}
+}
+
+final class UnfairLock: Lock {
+	private let _lock: os_unfair_lock_t
+
+	init() {
+		_lock = .allocate(capacity: 1)
+		_lock.initialize(to: os_unfair_lock())
+	}
+
+	func lock() {
+		os_unfair_lock_lock(_lock)
+	}
+
+	func unlock() {
+		os_unfair_lock_unlock(_lock)
+	}
+
+	func trylock() -> Bool {
+		return os_unfair_lock_trylock(_lock)
+	}
+
+	deinit {
+		_lock.deinitialize(count: 1)
+		_lock.deallocate()
+	}
+}
+
+protocol Lock {
+	init()
+	func lock()
+	func unlock()
+	func trylock() -> Bool
+}
+
+extension Lock {
+
+	func with<A>(_ f: () -> A) -> A {
 		lock()
 		defer { unlock() }
-
-		return f(_value)
+		return f()
 	}
 }
